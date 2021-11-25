4509745629c58d1f99db33f549b0739d1822aa7b
MoonSeoJun <msj00130@gmail.com>
2021-11-25 13:33:08 +0900

    [ADD] register db

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@
 
 from flask import Flask
 
+def register_extension(app : Flask):
+    from app import extension
+    extension.db.init_app(app)
+
 def register_blueprint(app : Flask):
     from .view.posts import arts_blueprint
     app.register_blueprint(arts_blueprint)
@@ -10,5 +14,6 @@ def create_app():
     app = Flask(__name__)
 
     register_blueprint(app)
+    register_extension(app)
 
     return app
