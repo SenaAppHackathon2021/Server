bdd3c3fe55c9a5e88388a82738404b21049ed007
dreamjh20 <dreamjh20@gmail.com>
2021-11-25 13:34:43 +0900

    ADD: Register Extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 
+def register_extension(app : Flask):
+    from .extexsion import db
+    extexsion.db.init_app(app)
+
+def register_blueprint(app : Flask):
+    from .view.account import account_blueprint
+    app.register_blueprint(account_blueprint)
+
 def create_app():
     app = Flask(__name__)
 
-    @app.route("/")
-    def hello():
-        return "Hello World"
+    register_blueprint(app)
+    register_extension(app)
 
     return app
