6b7b1d7ff49e7de68ebd034ccdc48944b625166f
dreamjh20 <dreamjh20@gmail.com>
2021-11-25 16:11:22 +0900

    FEAT: Link Signup DB

--- app/view/account/signup.py
+++ app/view/account/signup.py
@@ -1,10 +1,16 @@
 # 회원가입 관련 API 작성
-from flask import request, jsonify
+from flask import json, request, jsonify
 from flask_restful import Resource
+from app.model.account import Account
 
 class Signup(Resource):
     def post(self):
         id = request.json['id']
         password = request.json['password']
         email = request.json['email']
-        return 
+        try:
+            Account.signup(id, password, email)
+        except:
+            return jsonify({"message" : "account create fail", "status_code" : "400"}})
+
+        return jsonify({"message" : "account created", "status_code" : "201"})
