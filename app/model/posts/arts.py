66ad193d4561ef176cc91644872b007291e8f05d
MoonSeoJun <msj00130@gmail.com>
2021-11-25 13:59:21 +0900

    [ADD] add arts basic logic

--- app/model/posts/arts.py
+++ app/model/posts/arts.py
@@ -1,1 +1,9 @@
 # 정크 아트 게시글 테이블 클래스와 메소드 작성
+from app.extension import db
+
+class ArtPost(db.Model):
+    __tablename__ = "Art_post"
+
+    @staticmethod
+    def find_all_arts():
+        return "Hello"
--- app/view/posts/arts.py
+++ app/view/posts/arts.py
@@ -1,9 +1,24 @@
 # 정크 아트 게시판 API 작성
+from flask import session
 from flask_restful import Resource
+
+from app.model.posts import arts
 
 class ArtPosts(Resource):
     def get(self):
+        check_login()
+
+        all_arts = arts.ArtPost.find_all_arts()
+
         return "Get", 200
 
     def post(self):
+
         return "Post", 200
+
+def check_login():
+    try:
+        with session['user']:
+            pass
+    except:
+        return "You need login", 401
