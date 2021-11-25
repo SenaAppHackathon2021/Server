6acd8d939766f8e957fe99108f3a77cf9c851f57
dreamjh20 <dreamjh20@gmail.com>
2021-11-25 16:12:59 +0900

    UPDATE: Config

--- config.py
+++ config.py
@@ -1,2 +1,12 @@
 mail_pw = 'reart1234'
 secret_key = 'reart'
+
+db = {
+    'user'     : 'root',
+    'password' : 'm-s-j-0-1-3-0',
+    'host'     : '127.0.0.1',
+    'port'     : '3306',
+    'database' : 'reartdb'
+}
+
+DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
